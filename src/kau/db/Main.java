cf4d22c0e2888ffdc5033980fcc0a255bd0ba7ec
Shinsungjun <ssjun511@gmail.com>
2020-11-07 14:50:49 +0900

    Table update 2

--- src/kau/db/Main.java
+++ src/kau/db/Main.java
@@ -1,8 +1,10 @@
 package kau.db;
 
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 	Window window = new Window();
     }
 }
--- src/kau/db/Window.java
+++ src/kau/db/Window.java
@@ -4,16 +4,20 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-public class Window extends JFrame{
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Window extends JFrame {
     JPanel attributeJpanel;
-    JCheckBox att1;
-    JCheckBox att2;
-    JCheckBox att3;
-    JCheckBox att4;
-    JCheckBox att5;
-    JCheckBox att6;
-    JCheckBox att7;
-    JCheckBox att8;
+    Connection con;
+    JCheckBox NameCb;
+    JCheckBox SsnCb;
+    JCheckBox BdateCb;
+    JCheckBox AddressCb;
+    JCheckBox SexCb;
+    JCheckBox SalaryCb;
+    JCheckBox SuperVisorCb;
+    JCheckBox DnameCb;
     JComboBox<String> departComboBox;
     JButton searchButton;
     JButton deleteButton;
@@ -25,17 +29,110 @@ public class Window extends JFrame{
     JLabel scopeText;
     JTable table;
     DefaultTableModel dtm;
+    Object header[];
+    Object content[][];
     String department[] = {"All", "Headquarters", "Administration", "Research"};
-    public Window() {
+    ArrayList<JCheckBox> attributeCbs;
+    public Window() throws SQLException {
         super("Company DB");
         setLayout(null);
+        connect_db();
+        test_db();
         add_checkbox_at_atrributeJpanel();
         add_buttons_at_frame();
         add_texts();
-        final Object header[] = {"Check","Name", "Ssn", "Bdate", "Address",
-                            "Sex", "Salary", "SuperVisor", "Dname"};
-        Object content[][] = {{"false","John Smith", "123456789","1965-01-09","731 Fondren, Houston, TX",
-                            "M", "30000.00", "Franklin Wong", "Research"},
+        init_table();
+        //frame 크기지정
+        setSize(1000, 500);
+        //창보이게 하기
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    void test_db() throws SQLException{
+        String stmt1 = "select Lname, Salary from EMPLOYEE";
+        PreparedStatement p = con.prepareStatement(stmt1);
+        ResultSet r = p.executeQuery();
+
+        while(r.next()){
+            String lname = r.getString(1);
+            String salary = r.getString(2);
+            System.out.println(lname +" " + salary);
+        }
+        try {
+            if (con != null)
+                con.close();
+        } catch (SQLException e) {
+        }
+    }
+    void connect_db() {
+        con = null;
+        // 연결
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //JDBC 드라이버 연결
+
+            // 접속 url과 사용자, 비밀번호
+            String url="jdbc:mysql://localhost:3306/company?serverTimezone=UTC";
+            String user="root";
+            String pwd="1234";
+
+            con = DriverManager.getConnection(url, user, pwd);
+            System.out.println("정상적으로 연결되었습니다.");
+
+        } catch (SQLException e) {
+            System.err.println("연결할 수 없습니다.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.err.println("드라이버를 로드할 수 없습니다.");
+            e.printStackTrace();
+        }
+    }
+
+    public void add_texts(){
+        scopeText = new JLabel("Scope");
+        this.add(scopeText);
+        scopeText.setBounds(15, 15, 100, 20);
+        newSalaryText = new JLabel("New Salary :");
+        this.add(newSalaryText);
+        newSalaryText.setBounds(525, 420, 100, 20);
+        numberOfPeopleText = new JLabel("Number of People :");
+        this.add(numberOfPeopleText);
+        numberOfPeopleText.setBounds(30, 410, 200, 20);
+        chooseText = new JLabel("Choose :");
+        this.add(chooseText);
+        chooseText.setBounds(30,430,100,20);
+    }
+
+    public void add_checkbox_at_atrributeJpanel(){
+        attributeJpanel = new JPanel();
+        attributeJpanel.setLayout(new GridLayout(2, 4));
+        attributeCbs = new ArrayList<>();
+        NameCb = new JCheckBox("Name", true);
+        SsnCb = new JCheckBox("Ssn", true);
+        BdateCb = new JCheckBox("Bdate", true);
+        AddressCb = new JCheckBox("Address", true);
+        SexCb = new JCheckBox("Sex", true);
+        SalaryCb = new JCheckBox("Salary", true);
+        SuperVisorCb = new JCheckBox("SuperVisor", true);
+        DnameCb = new JCheckBox("Dname", true);
+        attributeCbs.add(NameCb);
+        attributeCbs.add(SsnCb);
+        attributeCbs.add(BdateCb);
+        attributeCbs.add(AddressCb);
+        attributeCbs.add(SexCb);
+        attributeCbs.add(SalaryCb);
+        attributeCbs.add(SuperVisorCb);
+        attributeCbs.add(DnameCb);
+        for(int i = 0; i < attributeCbs.size(); i++){
+            attributeJpanel.add(attributeCbs.get(i));
+        }
+        this.add(attributeJpanel);
+        attributeJpanel.setBounds(280,15,500,70);
+    }
+    public void init_table(){
+        header = new Object[]{"Check", "Name", "Ssn", "Bdate", "Address",
+                "Sex", "Salary", "SuperVisor", "Dname"}; //값을 받아서 넘어온 애들로만 만들기
+        content = new Object[][]{{"false","John Smith", "123456789","1965-01-09","731 Fondren, Houston, TX",
+                "M", "30000.00", "Franklin Wong", "Research"},
                 {"true","John SS", "123456789","1965-01-09","731 Fondren, Houston, TX",
                         "M", "30000.00", "Franklin Wong", "Research"}};
         dtm = new DefaultTableModel(content, header){
@@ -49,13 +146,60 @@ public class Window extends JFrame{
         table.getColumnModel().getColumn(0).setCellRenderer(new TableCell());
         table.getColumnModel().getColumn(0).setCellEditor(new TableCell());
         JScrollPane scrollpane = new JScrollPane(table);
-        add(scrollpane);
+        this.add(scrollpane);
         scrollpane.setBounds(20,90,960,310);
-        //frame 크기지정
-        setSize(1000,500);
-        //창보이게 하기
-        setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void create_table(ArrayList<String> attributeHeader) {
+        header = new Object[]{"Check", "Name", "Ssn", "Bdate", "Address",
+                "Sex", "Salary", "SuperVisor", "Dname"}; //값을 받아서 넘어온 애들로만 만들기
+        header = attributeHeader.toArray();
+        content = new Object[][]{{"false","John Smith", "123456789","1965-01-09","731 Fondren, Houston, TX",
+                "M", "30000.00", "Franklin Wong", "Research"},
+                {"true","John SS", "123456789","1965-01-09","731 Fondren, Houston, TX",
+                        "M", "30000.00", "Franklin Wong", "Research"}};
+        dtm = new DefaultTableModel(content, header){
+            public boolean isCellEditable(int i, int c){
+                //if(c == 0) return true;
+                //return false;
+                return true;
+            }
+        };
+        table = new JTable(dtm);
+        table.getColumnModel().getColumn(0).setCellRenderer(new TableCell());
+        table.getColumnModel().getColumn(0).setCellEditor(new TableCell());
+        JScrollPane scrollpane = new JScrollPane(table);
+        this.add(scrollpane);
+        scrollpane.setBounds(20,90,960,310);
+    }
+    public void add_buttons_at_frame(){
+        departComboBox = new JComboBox<String>(department);
+        this.add(departComboBox);
+        departComboBox.setBounds(10,35,200,30);
+        searchButton = new JButton("Search");
+        searchButton.setBounds(820, 28, 100,40);
+        ArrayList<String> header = new ArrayList<>();
+        searchButton.addActionListener(e -> {
+            //모든 attribute 모두 조회 후 check 된 것만 보여주는 식으로.
+            header.clear();
+            header.add("Check");
+            for(int i =0; i<attributeCbs.size(); i++){
+                if(attributeCbs.get(i).isSelected()){
+                    header.add(attributeCbs.get(i).getText());
+                }
+            }
+            create_table(header);
+        });
+
+        this.add(searchButton);
+        deleteButton = new JButton("Delete");
+        deleteButton.setBounds(820, 410, 100,40);
+        this.add(deleteButton);
+        updateButton = new JButton("Update");
+        updateButton.setBounds(700, 410, 100, 40);
+        this.add(updateButton);
+        salaryTextField = new JTextField(10);
+        salaryTextField.setBounds(600, 410, 100, 40);
+        this.add(salaryTextField);
     }
     class TableCell extends AbstractCellEditor implements TableCellEditor, TableCellRenderer{
         JCheckBox employee_check;
@@ -80,60 +224,5 @@ public class Window extends JFrame{
         public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
             return employee_check;
         }
-    }
-    public void add_texts(){
-        scopeText = new JLabel("Scope");
-        this.add(scopeText);
-        scopeText.setBounds(15, 15, 100, 20);
-        newSalaryText = new JLabel("New Salary :");
-        this.add(newSalaryText);
-        newSalaryText.setBounds(525, 420, 100, 20);
-        numberOfPeopleText = new JLabel("Number of People :");
-        this.add(numberOfPeopleText);
-        numberOfPeopleText.setBounds(30, 410, 200, 20);
-        chooseText = new JLabel("Choose :");
-        this.add(chooseText);
-        chooseText.setBounds(30,430,100,20);
-    }
-
-    public void add_checkbox_at_atrributeJpanel(){
-        attributeJpanel = new JPanel();
-        attributeJpanel.setLayout(new GridLayout(2, 4));
-        att1 = new JCheckBox("Name", true);
-        att2 = new JCheckBox("Ssn", true);
-        att3 = new JCheckBox("Bdate", true);
-        att4 = new JCheckBox("Address", true);
-        att5 = new JCheckBox("Sex", true);
-        att6 = new JCheckBox("Salary", true);
-        att7 = new JCheckBox("SuperVisor", true);
-        att8 = new JCheckBox("Dname", true);
-        attributeJpanel.add(att1);
-        attributeJpanel.add(att2);
-        attributeJpanel.add(att3);
-        attributeJpanel.add(att4);
-        attributeJpanel.add(att5);
-        attributeJpanel.add(att6);
-        attributeJpanel.add(att7);
-        attributeJpanel.add(att8);
-        this.add(attributeJpanel);
-        attributeJpanel.setBounds(280,15,500,70);
-    }
-
-    public void add_buttons_at_frame(){
-        departComboBox = new JComboBox<String>(department);
-        this.add(departComboBox);
-        departComboBox.setBounds(10,35,200,30);
-        searchButton = new JButton("Search");
-        searchButton.setBounds(820, 28, 100,40);
-        this.add(searchButton);
-        deleteButton = new JButton("Delete");
-        deleteButton.setBounds(820, 410, 100,40);
-        this.add(deleteButton);
-        updateButton = new JButton("Update");
-        updateButton.setBounds(700, 410, 100, 40);
-        this.add(updateButton);
-        salaryTextField = new JTextField(10);
-        salaryTextField.setBounds(600, 410, 100, 40);
-        this.add(salaryTextField);
     }
 }
