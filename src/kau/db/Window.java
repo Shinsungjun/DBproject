69f1956938636746653c3e7040d6a32afa111085
Shinsungjun <ssjun511@gmail.com>
2020-11-07 17:36:42 +0900

    before Check box...fix

--- src/kau/db/Window.java
+++ src/kau/db/Window.java
@@ -24,6 +24,7 @@ public class Window extends JFrame {
     JButton updateButton;
     JTextField salaryTextField;
     JLabel chooseText;
+    JLabel numberofPeople;
     JLabel numberOfPeopleText;
     JLabel newSalaryText;
     JLabel scopeText;
@@ -34,49 +35,21 @@ public class Window extends JFrame {
     String department[] = {"All", "Headquarters", "Administration", "Research"};
     ArrayList<JCheckBox> attributeCbs;
     ArrayList<ArrayList<String>> contents;
+    ArrayList<Boolean> isAttributeChecked;
     public Window() throws SQLException {
         super("Company DB");
         setLayout(null);
         connect_db();
-        test_db();
+        init_table_db();
+        init_table();
         add_checkbox_at_atrributeJpanel();
         add_buttons_at_frame();
         add_texts();
-        contents = new ArrayList<ArrayList<String>>();
-        init_table_db();
-        init_table();
         //frame 크기지정
         setSize(1000, 500);
         //창보이게 하기
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    void init_table_db() throws SQLException{
-        String stmt1 = "select distinct CONCAT(employee.fname,\" \",employee.minit,\" \",employee.lname)name, employee.ssn, employee.Bdate, employee.Address, employee.sex, employee.salary," +
-                " if(employee.super_ssn is NULL, 'NULL' , CONCAT(mgr.fname,\" \",mgr.minit,\" \",mgr.lname))supermgr_name, dname \n" +
-                "FROM employee, employee as mgr, department \n" +
-                "WHERE department.dnumber =  employee.dno and (mgr.ssn = employee.super_ssn or employee.super_ssn is NULL)";
-        PreparedStatement s = con.prepareStatement(stmt1);
-        ResultSet r = s.executeQuery();
-        while(r.next()) {
-            ArrayList<String> temp = new ArrayList<>();
-            temp.add("false");
-            for (int i = 1; i < 9; i++) {
-                temp.add(r.getString(i));
-            }
-            contents.add(temp);
-        }
-    }
-    void test_db() throws SQLException{
-        String stmt1 = "select Lname, Salary from EMPLOYEE";
-        PreparedStatement p = con.prepareStatement(stmt1);
-        ResultSet r = p.executeQuery();
-
-        while(r.next()) {
-            String lname = r.getString(1);
-            String salary = r.getString(2);
-            System.out.println(lname + " " + salary);
-        }
     }
     void connect_db() {
         con = null;
@@ -100,6 +73,73 @@ public class Window extends JFrame {
             e.printStackTrace();
         }
     }
+    void init_table_db() throws SQLException{
+        contents = new ArrayList<ArrayList<String>>();
+        String stmt1 = "select distinct CONCAT(employee.fname,\" \",employee.minit,\" \",employee.lname)name, employee.ssn, employee.Bdate, employee.Address, employee.sex, employee.salary," +
+                " if(employee.super_ssn is NULL, 'NULL' , CONCAT(mgr.fname,\" \",mgr.minit,\" \",mgr.lname))supermgr_name, dname \n" +
+                "FROM employee, employee as mgr, department \n" +
+                "WHERE department.dnumber =  employee.dno and (mgr.ssn = employee.super_ssn or employee.super_ssn is NULL)";
+        PreparedStatement s = con.prepareStatement(stmt1);
+        ResultSet r = s.executeQuery();
+        while(r.next()) {
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add("false");
+            for (int i = 1; i < 9; i++) {
+                temp.add(r.getString(i));
+            }
+            contents.add(temp);
+        }
+        String counter = "select count(*) from employee";
+        PreparedStatement NumberOfEmployee = con.prepareStatement(counter);
+        ResultSet c = NumberOfEmployee.executeQuery();
+        while(c.next()) {
+            numberofPeople = new JLabel(c.getString(1));
+        }
+    }
+    void test_db() throws SQLException{
+        String stmt1 = "select Lname, Salary from EMPLOYEE";
+        PreparedStatement p = con.prepareStatement(stmt1);
+        ResultSet r = p.executeQuery();
+
+        while(r.next()) {
+            String lname = r.getString(1);
+            String salary = r.getString(2);
+            System.out.println(lname + " " + salary);
+        }
+    }
+    void set_table_db(int total_select) throws SQLException {
+        contents.clear();
+        String selectStmt[] = new String[]{"CONCAT(employee.fname,\" \",employee.minit,\" \",employee.lname)name,", "employee.ssn,","employee.Bdate,",
+        "employee.Address,", "employee.sex,", "employee.salary,", "if(employee.super_ssn is NULL, 'NULL' , CONCAT(mgr.fname,\" \",mgr.minit,\" \",mgr.lname))supermgr_name, " , "dname \n"};
+        String whereDepartment[] = new String[]{"department.dnumber =  employee.dno", "department.dnumber = employee.dno and employee.dno = 1",
+                "department.dnumber = employee.dno and employee.dno = 4","department.dnumber = employee.dno and employee.dno = 5"};
+        String stmt = "";
+        for(int i = 0; i < isAttributeChecked.size(); i++) {
+            if (isAttributeChecked.get(i) && i != 7) {
+                stmt = stmt + selectStmt[i];
+            }
+            if(i == 7){
+                stmt = stmt + selectStmt[i];
+            }
+        }
+
+        String stmt1 = "SELECT distinct " + stmt +
+                "FROM employee, employee as mgr, department \n" +
+                "WHERE " + whereDepartment[departComboBox.getSelectedIndex()] + " and (mgr.ssn = employee.super_ssn or employee.super_ssn is NULL)";
+
+        PreparedStatement p = con.prepareStatement(stmt1);
+        ResultSet r = p.executeQuery();
+        while(r.next()) {
+            ArrayList<String> temp = new ArrayList<>();
+            temp.add("false");
+            for (int i = 1; i < total_select + 1; i++) {
+                temp.add(r.getString(i));
+            }
+            contents.add(temp);
+        }
+        numberofPeople.setText(String.valueOf(contents.size()));
+    }
+
 
     public void add_texts(){
         scopeText = new JLabel("Scope");
@@ -108,9 +148,11 @@ public class Window extends JFrame {
         newSalaryText = new JLabel("New Salary :");
         this.add(newSalaryText);
         newSalaryText.setBounds(525, 420, 100, 20);
-        numberOfPeopleText = new JLabel("Number of People :");
+        numberOfPeopleText = new JLabel("Number of People : ");
         this.add(numberOfPeopleText);
         numberOfPeopleText.setBounds(30, 410, 200, 20);
+        numberofPeople.setBounds(155, 410,200,20);
+        this.add(numberofPeople);
         chooseText = new JLabel("Choose :");
         this.add(chooseText);
         chooseText.setBounds(30,430,100,20);
@@ -164,18 +206,16 @@ public class Window extends JFrame {
         scrollpane.setBounds(20,90,960,310);
     }
     public void create_table(ArrayList<String> attributeHeader) {
-        header = new Object[]{"Check", "Name", "Ssn", "Bdate", "Address",
-                "Sex", "Salary", "SuperVisor", "Dname"}; //값을 받아서 넘어온 애들로만 만들기
+         //값을 받아서 넘어온 애들로만 만들기
         header = attributeHeader.toArray();
-        content = new Object[][]{{"false","John Smith", "123456789","1965-01-09","731 Fondren, Houston, TX",
-                "M", "30000.00", "Franklin Wong", "Research"},
-                {"true","John SS", "123456789","1965-01-09","731 Fondren, Houston, TX",
-                        "M", "30000.00", "Franklin Wong", "Research"}};
+        content = new Object[contents.size()][];
+        for(int i =0; i<contents.size(); i++){
+            content[i] = contents.get(i).toArray();
+        }
         dtm = new DefaultTableModel(content, header){
             public boolean isCellEditable(int i, int c){
-                //if(c == 0) return true;
-                //return false;
-                return true;
+                if(c == 0) return true;
+                return false;
             }
         };
         table = new JTable(dtm);
@@ -187,19 +227,31 @@ public class Window extends JFrame {
     }
     public void add_buttons_at_frame(){
         departComboBox = new JComboBox<String>(department);
+        isAttributeChecked = new ArrayList<>();
         this.add(departComboBox);
         departComboBox.setBounds(10,35,200,30);
         searchButton = new JButton("Search");
         searchButton.setBounds(820, 28, 100,40);
         ArrayList<String> header = new ArrayList<>();
         searchButton.addActionListener(e -> {
+            isAttributeChecked.clear();
             //모든 attribute 모두 조회 후 check 된 것만 보여주는 식으로.
             header.clear();
             header.add("Check");
+            int total_select = 0;
             for(int i =0; i<attributeCbs.size(); i++){
                 if(attributeCbs.get(i).isSelected()){
                     header.add(attributeCbs.get(i).getText());
+                    isAttributeChecked.add(true);
+                    total_select++;
                 }
+                else isAttributeChecked.add(false);
+            }
+            //db 조회를 다시.
+            try {
+                set_table_db(total_select);
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
             create_table(header);
         });
@@ -220,7 +272,7 @@ public class Window extends JFrame {
         public TableCell(){
             employee_check = new JCheckBox();
             employee_check.addActionListener(e -> {
-                System.out.println(table.getValueAt(table.getSelectedRow(), 1));
+                System.out.println(table.getValueAt(table.getSelectedRow(), 2));
             });
         }
         @Override
